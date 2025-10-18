adf922198c415e04c692dd6ed7d532a7eab379af
tetsuya-stn <t.hitotsuishi@gmail.com>
2025-10-18 22:41:26 +0900

    section 1.1, 1.2

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tetsuya-stn/go-api-server-handson
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/tetsuya-stn/go-api-server-handson/handlers"
+)
+
+func main() {
+	http.HandleFunc("/hello", handlers.HelloHandler)
+	http.HandleFunc("/article", handlers.PostArticleHandler)
+	http.HandleFunc("/article/list", handlers.ArticleListHandler)
+	http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+	http.HandleFunc("/article/nice", handlers.PostNiceHandler)
+	http.HandleFunc("/comment", handlers.PostCommentHandler)
+
+	log.Println("server start at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
